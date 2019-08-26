5b9863fd2a762e3d8591d070792ad7e22e07106c
Ridwan Balogun <balogunridwan@gmail.com>
2019-08-26 19:51:17 +0100

    add edit profile

--- src/util/AuthRoute.js
+++ src/util/AuthRoute.js
@@ -7,15 +7,17 @@ import PropTypes from 'prop-types'
 const AuthRoute = ({component: Component, authenticated, ...rest}) => (
     <Route 
     {...rest}
-    render={(props)=> authenticated === true ? <Redirect to='/'/> : <Component {...props}/>}
+    render={(props) => 
+        authenticated === true ? <Redirect to="/" /> : <Component {...props} />
+    }
     />
-)
+);
  
 const mapStateToProps = (state) => ({
     authenticated: state.user.authenticated
-})
+});
 
 AuthRoute.propTypes = {
-    user: PropTypes.object.isRequired
-}
+    user: PropTypes.object
+};
 export default connect(mapStateToProps)(AuthRoute);
